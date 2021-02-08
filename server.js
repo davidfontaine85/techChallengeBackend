792b343579475e1cb89dbe18551440be9bd0508a
David Fontaine <david.fontaine85@gmail.com>
2021-02-08 11:37:34 +0100

    change to process.env.PORT

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.post('/api/insert', (req, res) => {
     });
 });
 
-app.listen(3001, ()=> {
-    console.log('Running on port 3001');
+app.listen(process.env.PORT || PORT, ()=> {
+    console.log('Running on port ${PORT}');
 });
